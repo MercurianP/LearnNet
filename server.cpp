79c06375114c7e5b347f0e39fd96870909bf1643
namzhang <494945533@qq.com>
2021-12-15 18:29:38 +0800

    build learning workspace

---
+++ server.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "hello world" << endl;
+    int i =0;
+    for(int i = 0 ;i < 10 ; i ++){
+        cout<<i<<endl;
+
+        cout<<i-1<<endl;
+    }
+    return 1;
+}
